47cfc04f94f9c2c407542d1c3b0ea7a7c30b5d3d
web-dev-a <61269281+web-dev-a@users.noreply.github.com>
2020-12-31 12:19:16 +0200

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,15 +1,3 @@
-const path = require("path")
-const slug = require("slug")
-
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-
-  // Add slug for page generation.
-
-}
-
-
-
 // Allow client-only routes on /admin/*
 exports.onCreatePage = async ({ page, actions }) => {
   if (page.path.match(/^\/admin/)) {
