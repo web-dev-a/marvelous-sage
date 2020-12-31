237acd739ddda9f0ffb9c4490a5bf3ef0cd59b64
web-dev-a <61269281+web-dev-a@users.noreply.github.com>
2020-12-31 12:06:05 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,14 +27,5 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-source-stripe`,
-      options: {
-        objects: ["Product", "Sku"],
-        secretKey: process.env.STRIPE_SECRET_KEY,
-        downloadFiles: true,
-        auth: false,
-      },
-    },
   ],
 }
